9415d3bd605aaf71fa108e367e696f3dfb488d6f
Miguel <miguel.alba@usantotomas.edu.co>
2020-08-24 00:13:51 +0200

    training and tools

---
+++ net_tools.py
@@ -0,0 +1,171 @@
+import os
+import glob
+import math
+import random
+
+import skimage
+import imgaug.augmenters as iaa
+
+import numpy as np
+import tensorflow as tf
+
+
+def overexpose(image):
+    image = skimage.exposure.equalize_hist(image.astype(np.float32))
+    image = skimage.exposure.adjust_gamma(image, 0.3)
+    return image
+
+
+def blur_image(image, blur_sigma):
+    return skimage.filters.gaussian(image, blur_sigma)
+        
+
+def external_augmentation(crop):
+    crop = crop.numpy()
+    sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+    seq = iaa.Sequential([sometimes(
+        iaa.Affine(scale=(1.0, 1.1),
+                   translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)},
+                   rotate=(-8, 8),
+                   shear=(-8, 8),
+                   mode='edge'))])
+
+    # crop = overexpose(crop) if random.random() < 0.2 else crop
+
+    # if random.random() < 0.1:
+        # blur_sigma = random.uniform(0, 1.5)
+        # crop = blur_image(crop, blur_sigma)
+
+    crop = seq.augment_image(crop)
+    return crop
+
+
+class DataLoader(object):
+    def __init__(self, data_path, training):
+        self.data_path = data_path
+        self.training = 'train' if training else 'test'
+        self.classes = ['NORMAL', 'CNV', 'DME', 'DRUSEN']
+        self.seed = 1
+        if self.training == 'train':
+            self.batch_size = 128
+            self.buffer = 4000
+        else:
+            self.batch_size = 128
+            self.buffer = 100
+
+    def parse_record(self, record):
+        features = {
+            'image': tf.io.FixedLenFeature([], dtype=tf.string),
+            'height': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'width': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'label': tf.io.FixedLenFeature([], dtype=tf.int64),
+        }
+        record = tf.io.parse_single_example(record, features)
+        img = tf.io.decode_raw(record['image'], tf.float32)
+        img = tf.reshape(img, [record['height'], record['width'], 1])
+        label = tf.one_hot(record['label'], len(self.classes), dtype=tf.float32)
+        return img, label
+
+    def random_jitteing(self, crop):
+        crop = tf.image.resize(crop, [78, 110], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        crop = tf.image.random_crop(crop, size=[68, 100, 1])
+        return crop
+
+    def agumentation(self, crop, label):
+        choise = tf.random.uniform(shape=[], minval=0., maxval=1., dtype=tf.float32)
+        shape = crop.get_shape()
+        crop = tf.image.random_flip_left_right(crop)
+        crop = tf.py_function(external_augmentation, inp=[crop], Tout=tf.float32)
+        crop.set_shape(shape)
+        crop = tf.cond(choise < 0.5, lambda: crop, lambda: self.random_jitteing(crop))
+        return crop, label
+
+    def load_dataset(self, label):
+        files = os.path.join(self.data_path, '{}_{}*.tfrecord'.format(self.training, label))
+        filenames = glob.glob(files)
+        dataset = tf.data.Dataset.list_files(files, shuffle=True, seed=self.seed)
+        dataset = dataset.interleave(lambda fn: tf.data.TFRecordDataset(fn), cycle_length=len(filenames),
+                                     num_parallel_calls=min(len(filenames), tf.data.experimental.AUTOTUNE))
+        dataset = dataset.map(self.parse_record, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        if self.training == 'train':
+            dataset = dataset.map(self.agumentation, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.shuffle(self.buffer, seed=self.seed)
+        dataset = dataset.repeat()
+        return dataset
+
+    def balanced_batch(self):
+        datasets = []
+        for cl in self.classes:
+            datasets.append(self.load_dataset(cl))
+        importance = [0.25, 0.25, 0.25, 0.25]
+        sampled_dataset = tf.data.experimental.sample_from_datasets(datasets, weights=importance)
+        sampled_dataset = sampled_dataset.batch(self.batch_size)
+        sampled_dataset = sampled_dataset.prefetch(2)
+        return sampled_dataset
+
+    def test_dataset(self, dataset_name=None):
+        """
+        :param dataset_name: take an specific dataset to evaluate
+        :return: all test dataset
+        """
+        if dataset_name:
+            files = os.path.join(self.data_path, 'test_*-{}_*.tfrecord'.format(dataset_name))
+        else:
+            files = os.path.join(self.data_path, 'test_*.tfrecord')
+        filenames = glob.glob(files)
+        dataset = tf.data.Dataset.list_files(files, shuffle=True, seed=self.seed)
+        dataset = dataset.interleave(lambda fn: tf.data.TFRecordDataset(fn), cycle_length=len(filenames),
+                                     num_parallel_calls=min(len(filenames), tf.data.experimental.AUTOTUNE))
+        dataset = dataset.map(self.parse_record, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.repeat(1)
+        dataset = dataset.batch(self.batch_size)
+        return dataset
+
+
+class Checkpoint:
+    def __init__(self, checkpoint_kwargs, out_dir, max_to_keep=5, keep_checkpoint_every_n_hours=None):
+        self.checkpoint = tf.train.Checkpoint(**checkpoint_kwargs)
+        self.manager = tf.train.CheckpointManager(self.checkpoint, out_dir, max_to_keep, keep_checkpoint_every_n_hours)
+
+    def restore(self, save_path=None):
+        save_path = self.manager.latest_checkpoint if save_path is None else save_path
+        return self.checkpoint.restore(save_path)
+
+    def save(self, file_prefix_or_checkpoint_number=None, session=None):
+        if isinstance(file_prefix_or_checkpoint_number, str):
+            return self.checkpoint.save(file_prefix_or_checkpoint_number, session=session)
+        else:
+            return self.manager.save(checkpoint_number=file_prefix_or_checkpoint_number)
+
+    def __getattr__(self, attr):
+        if hasattr(self.checkpoint, attr):
+            return getattr(self.checkpoint, attr)
+        elif hasattr(self.manager, attr):
+            return getattr(self.manager, attr)
+        else:
+            self.__getattribute__(attr)
+
+
+def write_tensorboard(stats_dict, step, full_eval=False):
+    name = 'Epoch metrics' if full_eval else 'Metrics'
+    type = stats_dict['type']
+    for scope, metric in stats_dict.items():
+        if scope == 'loss':
+            tf.summary.scalar('{}/Loss'.format(name), metric.numpy(), step)
+        if scope == 'average_loss':
+            tf.summary.scalar('{}/Average Loss'.format(name), metric.numpy(), step)
+        if scope == 'accuracy':
+            tf.summary.scalar('{}/Accuracy'.format(name), metric.numpy(), step)
+
+
+def build_graph(model, feats, log_dir, step=0):
+    @tf.function
+    def tracing(feats):
+        pred = model(feats)
+        return pred
+
+    writer = tf.summary.create_file_writer(os.path.join(log_dir, 'model_graph'))
+    tf.summary.trace_on(graph=True)
+    _ = tracing(feats)
+    with writer.as_default():
+        tf.summary.trace_export(name="graphs", step=step)
--- training.py
+++ training.py
@@ -1,4 +1,203 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
 
+import utils
+from net_tools import DataLoader, Checkpoint, write_tensorboard, build_graph
+from conv_nets import models
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
+
+class OCTtraining(object):
+    def __init__(self, modelname, data_path, architecture, hyperparams, img_size=[136, 136], **kwargs):
+        """
+        :param modelname: model name
+        :param data_path: folder of the data records
+        :param model: tensorflow model
+        :param hyperparams: params
+        :param kwargs:
+        """
+        # initialize GPUs
+        utils.setup_gpus()
+        self.modelname = modelname
+        self.model_path = os.path.join('./trained_models', modelname)
+        self.data_path = data_path
+        # hyper parameter
+        self.params = {
+            'batch_size': 128,
+            'learning_rate': 0.001,
+            'schedule': False,  # adaptive learning rate
+            'optimizer': 'ADAM',  # SGD, SGDM
+            'test_iter': 100,
+            'step_size': 2000,
+            'epochs': 50,
+            'max_class_samples': 51140  # the second time I see again a NORMAL sample
+        }
+        self.params.update(hyperparams)
+        self.img_size = img_size
+        self.log_dir, self.ckpt_dir, self.train_writer, self.test_writer = self.create_dirs()
+
+        # steps per epoch
+        self.steps_epoch = np.ceil(2.0 * self.params['max_class_samples'] / self.params['batch_size'])
+        self.epochs = self.params['epochs']
+        self.epoch_counter = tf.Variable(initial_value=0, trainable=False, dtype=tf.int64)
+        self.step = tf.Variable(initial_value=0, trainable=False, dtype=tf.int64)
+
+        self.architecure_params = dict(**kwargs)
+        self.model = self.build_model(architecture, **self.architecure_params)
+
+        self.train_data = DataLoader(self.data_path, training=True)
+        self.test_data = DataLoader(self.data_path, training=False)
+
+        self.lr, self.opt = self.optimizer()
+        self.loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
+
+        self.train_loss, self.test_loss, self.train_acc, self.test_acc = self.build_metrics()
+
+        architecture = dict(model=self.model,
+                            optimizer=self.opt,
+                            current_epoch=self.epoch_counter,
+                            step=self.step)
+        self.ckpt = Checkpoint(architecture, self.ckpt_dir, max_to_keep=3)
+        try:
+            self.ckpt.restore().assert_existing_objects_matched()
+            print('Loading pre trained model')
+        except Exception as e:
+            print(e)
+
+    def create_dirs(self):
+        log_dir = os.path.join(self.model_path, 'logs')
+        ckpt_dir = os.path.join(self.model_path, 'checkpoints')
+        train_writer = tf.summary.create_file_writer(os.path.join(log_dir, 'opt/train'))
+        test_writer = tf.summary.create_file_writer(os.path.join(log_dir, 'opt/test'))
+        utils.mdir(log_dir)
+        utils.mdir(ckpt_dir)
+        utils.mdir(os.path.join(self.model_path, 'weights'))
+        return log_dir, ckpt_dir, train_writer, test_writer
+
+    def build_model(self, architecture, **params):
+        input_shape = [self.img_size[0], self.img_size[1], 3]
+        model = getattr(models, str(architecture))(name=self.modelname, **params)
+        try:
+            print(model.summary())
+        except ValueError:
+            inputs = tf.keras.Input(shape=tuple(input_shape), name='input_img')
+            model(inputs)
+            print('== Model description ==')
+            print(model.summary())
+        return model
+
+    def optimizer(self):
+        if self.params['schedule']:
+            lr = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=self.params['learning_rate'],
+                                                                decay_steps=self.params['step_size'],
+                                                                decay_rate=0.5, staircase=True)
+        else:
+            lr = self.params['learning_rate']
+        if self.params['optimizer'] == 'SGDM':
+            opt = tf.keras.optimizers.SGD(learning_rate=lr, momentum=0.9)
+        elif self.params['optimizer'] == 'ADAM':
+            opt = tf.keras.optimizers.Adam(learning_rate=lr)
+        else:
+            raise NotImplementedError
+        return lr, opt
+
+    def build_metrics(self):
+        train_loss = tf.keras.metrics.Mean(name='train_loss')
+        test_loss = tf.keras.metrics.Mean(name='test_loss')
+        train_acc = tf.keras.metrics.CategoricalAccuracy(name='train_acc')
+        test_acc = tf.keras.metrics.CategoricalAccuracy(name='test_acc')
+        return train_loss, test_loss, train_acc, test_acc
+
+    def cat_cross_entropy(self, data, labels, training):
+        predictions = self.model(data, training)
+        obj_loss = self.loss(y_true=labels, y_pred=predictions)
+        return obj_loss
+
+    @tf.function
+    def train_step(self, data, labels):
+        with tf.GradientTape() as tape:
+            predictions = self.model(data, training=True)
+            loss = self.cat_cross_entropy(data, labels, training=True)
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.opt.apply_gradients(zip(gradients, self.model.trainable_variables))  # update
+        _loss = self.train_loss(loss)
+        _acc = self.train_acc(labels, predictions)
+        summary = {'type': 'train', 'loss': loss, 'average_loss': _loss, 'accuracy': _acc}
+        return summary
+
+    @tf.function
+    def test_step(self, data, labels):
+        predictions = self.model(data, training=False)
+        loss = self.cat_cross_entropy(data, labels, training=False)
+        _loss = self.test_loss(loss)
+        _acc = self.test_acc(labels, predictions)
+        summary = {'type': 'test', 'loss': loss, 'average_loss': _loss, 'accuracy': _acc}
+        return summary
+
+    def train(self):
+        print('Starting Training')
+        train = self.train_data.balanced_batch()
+        test = self.test_data.balanced_batch()
+        data = tf.data.Dataset.zip((train, test))
+
+        epoch_bar = tqdm(total=self.epochs, desc='Epoch', position=0)
+        for epoch in range(int(self.epoch_counter), int(self.epochs)):
+            self.epoch_counter.assign_add(1)
+            step_bar = tqdm(total=self.steps_epoch, desc='Steps', position=1)
+            for train_batch, test_batch in data:
+                img, labels = train_batch
+                test_img, test_labels = test_batch
+                train_summary = self.train_step(img, labels)
+                test_summary = self.test_step(test_img, test_labels)
+                if int(self.step) == 0:
+                    build_graph(self.model, img, self.log_dir, self.step)
+                (train_loss, train_acc) = self.train_loss.result(), self.train_acc.result()
+                (test_loss, test_acc) = self.test_loss.result(), self.test_acc.result()
+                lr = self.lr(self.step).numpy() if self.params['schedule'] else self.lr
+                if int(self.step % self.params['test_iter']) == 0:
+                    with self.train_writer.as_default():
+                        write_tensorboard(train_summary, step=self.step)
+                        tf.summary.scalar('Metrics/Learning_rate', lr, step=self.step)
+                        self.train_loss.reset_states()
+                        self.train_acc.reset_states()
+                    with self.test_writer.as_default():
+                        write_tensorboard(test_summary, step=self.step)
+                        self.test_loss.reset_states()
+                        self.test_acc.reset_states()
+
+                self.step.assign_add(1)
+                step_bar.update(1)
+
+                if int(self.step % self.steps_epoch) == 0:
+                    with self.train_writer.as_default():
+                        write_tensorboard(train_summary, step=self.step, full_eval=True)
+                    break
+
+            epoch_bar.update(1)
+
+            for _test in self.test_data.test_dataset():
+                test_summary = self.test_step(_test[0], _test[1])
+            with self.test_writer.as_default():
+                write_tensorboard(test_summary, step=self.step, full_eval=True)
+
+            template = 'train loss: {}, test loss: {}, train acc: {}, test acc: {} <='
+            print(template.format(train_loss, test_summary['loss'], train_acc, test_summary['accuracy']))
+
+            self.train_writer.flush()
+            self.test_writer.flush()
+
+            self.ckpt.save(epoch)
+            # TODO: fix serialization with low level API
+            self.model.save_weights(os.path.join(self.model_path, 'weights', 'pretrained'),
+                                    overwrite=True, save_format='tf')
+            self.model.save(os.path.join(self.model_path, 'frozen'))
+            if int(self.step % (self.epochs * self.steps_epoch)) == 0:
+                break
+        print('Finished Training')
+        return None
 
 
 if __name__ == '__main__':
